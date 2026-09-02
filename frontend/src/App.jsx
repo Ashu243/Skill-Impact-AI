a61e0b4c0d6d3bb565825b5397cf7d1e8af5e714
Ashu <ashuv6309@gmail.com>
2026-09-02 21:47:17 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-2xl text-zinc-800'>
+      Hey There
+    </div>
+    </>
+  )
+}
+
+export default App
